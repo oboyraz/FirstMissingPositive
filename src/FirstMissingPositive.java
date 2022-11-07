4e3eccf116c4199f51f8038ddbefc81b576b7c29
omerboyraz <omer.boyraz@isuzu.com.tr>
2022-11-07 15:31:36 +0300

    firstMissingPositiveNumber

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -1,34 +1,36 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FirstMissingPositive {
     /*
     Given an unsorted integer array nums, return the smallest missing positive integer.
-
     You must implement an algorithm that runs in O(n) time and uses constant extra space.
+    to see question => https://leetcode.com/problems/first-missing-positive/
      */
     public static void main(String[] args) {
+        int[] arr= {3,4,-1,1};
         //int[] arr = {1,2,0};
-        int[] arr = {3,4,-1,1};
-        //int[] arr= {7,8,9,11,5,12};
+        //int[] arr= {7,8,9,11,12};
+        System.out.println(firstMissingPositive(arr));
+    }
+    private static int firstMissingPositive(int[] arr) {
+        List<Integer> arr1 = new ArrayList<>();
         Arrays.sort(arr);
         int lastNumber = arr[arr.length-1];
-        int index = arr.length-1;
-        boolean varYok = false;
-        int kayip = 0;
-        while (index>0){
-            if (lastNumber == arr[index]){
-                varYok = true;
+        int counter = 1;
+        boolean minPoz = true;
+        for (int i = 0; i < arr.length; i++) {
+            arr1.add(arr[i]);
+        }
+        for (int i = 0; i <arr1.size() ; i++) {
+            if (arr1.contains(counter)){
+                counter++;
             }
-            else {
-                varYok = false;
-            }
-            if (!varYok){
-                kayip = lastNumber;
+            else{
                 break;
             }
-            index--;
-            lastNumber--;
         }
-        System.out.println(kayip);
+        return counter;
     }
 }
